940c391b7c5b3624176e5d7d57f8af55945aa27d
nataliiaGrynda <nataliiagrynda@gmail.com>
2022-04-13 20:42:43 -0500

    added solution

--- src/Survey.java
+++ src/Survey.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Survey {
+    public class Survey {
     public static void main(String[] args) {
 
         /**
@@ -14,6 +16,55 @@ public class Survey {
         -Print the age of the youngest person
         -Print the age of the eldest person
          */
+        //global veriables or objects needed
+        Scanner inputReader = new Scanner(System.in);
+        ArrayList<Participant> participants = new ArrayList<>();
+       int youngest = Integer.MAX_VALUE, eldest = 0;
 
+
+        do{
+            System.out.println(SurveyQuestions.askToJoin);
+            String join = inputReader.next();
+
+            if(join.toUpperCase().startsWith("Y")){
+                Participant.addParticipants();
+
+                //ask your question to get name, age, gender
+                System.out.println(SurveyQuestions.askName);
+                String name = inputReader.next();
+
+                System.out.println(SurveyQuestions.askAge);
+                int age = inputReader.nextInt();
+
+                System.out.println(SurveyQuestions.askGender);
+                String gender = inputReader.next();
+
+               // Participant participant = new Participant();
+               // participant.name = name;
+              //  participant.age = age;
+              //  participant.gender = gender;
+
+      Participant participant = new Participant(name, age, gender);
+      participants.add(participant);
+
+                if(gender.toUpperCase().startsWith("M"))Participant.addMaleParticipants();
+                else Participant.addFemaleParticipants();
+
+            }
+
+        }while(Participant.totalNumberOfFemaleParticipants < 3);
+
+
+        for (Participant participant : participants) {
+            System.out.println(participant);
+            if(participant.age > eldest) eldest = participant.age;
+            if(participant.age < youngest) youngest = participant.age;
+        }
+
+
+        System.out.println("The total number of male participants = " + Participant.totalNumberOfMaleParticipants);
+        System.out.println("The total number of female participants = " + Participant.totalNumberOfFemaleParticipants);
+        System.out.println("The age of the youngest person is = " + youngest);
+        System.out.println("The age of the eldest person is = " + eldest);
     }
 }
